f90751f7d2562f2484cb90449f777a9e8b3b5759
sathish <rajsathishdev@gmail.com>
2020-08-16 20:31:16 +0530

    initial setup

---
+++ src/app/Patient.ts
@@ -0,0 +1,8 @@
+export class Patient {
+    id: number;
+    name: string;
+    address: string;
+    country: string;
+    status: string;
+    status_id: number;
+}
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PatientsListComponent } from './patients-list/patients-list.component';
+import { AddPatientComponent } from './add-patient/add-patient.component';
+import { EditPatientComponent } from './edit-patient/edit-patient.component';
+
+
+const routes: Routes = [
+   {
+     path: '/list',
+     component: PatientsListComponent
+   },
+   {
+     path: '/add',
+     component: AddPatientComponent
+   },
+   {
+     path: 'edit',
+     component: EditPatientComponent
+   },
+   {
+     path: '',
+     redirectTo: '/list'
+   }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
